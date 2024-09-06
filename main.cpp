cd00da1f3ac308470f26a913dba3759941139ccb
AiroZ4 <138257231+AiroZ4@users.noreply.github.com>
2024-09-06 10:17:11 -0400

    level drawing system
    You can now draw other levels and switch them from the current_level int. Still needs the player spawn in the level data

--- main.cpp
+++ main.cpp
@@ -12,27 +12,44 @@
 
 const int screenWidth = 800;
 const int screenHeight = 800;
+int current_level = 0;
+
 
 Player p;
 std::vector<Block> blocks;  // Vector to hold multiple blocks
+std::vector<Block> level_1;
+std::vector<Block> level_2;
 
 int main() {
     InitWindow(screenWidth, screenHeight, "Arcade Game");
     SetTargetFPS(60);
 
-    // Create some blocks
-    blocks.push_back(Block{ {200, 750}, {200, 750, 100, 15} });
-    blocks.push_back(Block{ {400, 700}, {400, 700, 100, 15} });
-    blocks.push_back(Block{ {300, 600}, {300, 600, 100, 15} });
+    // Level 1 Data
+    level_1.push_back(Block{ {155, 223}, {155, 223, 84, 42} });
+    level_1.push_back(Block{ {306, 182}, {306, 182, 66, 24} });
+    level_1.push_back(Block{ {307, 264}, {307, 264, 77, 28} });
+
+    // Level 2 Data
+    level_2.push_back(Block{ {107, 401}, {107, 401, 117, 24} });
+    level_2.push_back(Block{ {291, 371}, {291, 371, 49, 10} });
+    level_2.push_back(Block{ {239, 310}, {239, 310, 19, 31} });
+    level_2.push_back(Block{ {338, 275}, {338, 275, 196, 17} });
     
 
     while (!WindowShouldClose()) {
         // Gather all block rectangles for collision checking
         std::vector<Rectangle> blockRects;
-        for (const auto& block : blocks) {
-            blockRects.push_back(block.getRect());
+        if (current_level == 0){
+            for (const auto& block : level_1) {
+                blockRects.push_back(block.getRect());
+            }
         }
-
+        else if (current_level == 1){
+            for (const auto& block : level_2) {
+                blockRects.push_back(block.getRect());
+            }
+        }
+        
         // Draw everything
         BeginDrawing();
         ClearBackground(BLACK);
@@ -40,10 +57,18 @@ int main() {
         // Update player and check for collisions with all blocks
         p.update(blockRects);
 
-        // Draw all blocks
-        for (auto& block : blocks) {
-            block.draw();
+        // Draw the level depending on the current_level
+        if (current_level == 0){
+            for (auto& block : level_1) {
+                block.draw();
+            }
         }
+        else if (current_level == 1){
+            for (auto& block : level_2) {
+                block.draw();
+            }
+        }
+        
 
         EndDrawing();
     }
